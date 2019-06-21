a376ea4319515d9c80a4dced9445403d11f9b7a2
Alessandra <alessandra.francez@poli.ufrj.br>
2019-06-21 15:26:36 -0300

    Untested - Adjusted conditional logic

--- V1.js
+++ V1.js
@@ -2,13 +2,38 @@ const arquivo = require('C:\\Users\\User\\Downloads\\skill-ADCC---Accenture.json
 const dialogos = arquivo.dialog_nodes
 let fluxo = []
 
+//window.open('C:\\Users\\User\\Desktop\\Watson visualization\\test.html')
 // Cria string no formato mermaid
-//console.log(gerarGraph("A","Texto", "Condição","B","NodoB","NodoBB"))
-let gerarGraph = function (node, text, condition, conditionID, nextNode, nextNodeText)  {
+//console.log(gerarGraph("nodo","Texto", "Condição","B",))
+let gerarGraph = function (node, text, condition, conditionID, parent = "first")  {
+    return `${parent}{"${condition}"} --> ${conditionID}(<p>${text}</p>)
+    `
+    //${conditionID}{${condition}}`
     return `${node}[${text}] --> ${conditionID}{${condition}}
     ${conditionID} --> ${nextNode}[${nextNodeText}]
     `
-} 
+}
+
+
+let gerarTitle = function (title) {
+
+    return `graph TD
+title["<b>${title}</b>"]
+title-->first
+style title fill:#FFF,stroke:#FFF
+linkStyle 0 stroke:#FFF,stroke-width:0;
+` 
+}
+
+//console.log(gerarTitle("texto") + gerarGraph("nodo","Texto", "Condição","B",))
+
+// ILOVEYOUBAE <3
+
+//Título
+// title[<b>My Title</b>]
+// title-->node_2_1551391813552
+// style title fill:#FFF,stroke:#FFF
+// linkStyle 0 stroke:#FFF,stroke-width:0;
 
 for (let item of dialogos) {
 
@@ -30,10 +55,11 @@ for (let item of dialogos) {
     `)      
             }
         }
-        let total = ''
+        let total = gerarTitle(fluxo[0].title)
         for (let i = 0; i < fluxo.length; i++) {
             console.log(`node: ${fluxo[i].dialog_node}`)
             if (i+1 < fluxo.length) {
+                total = total + gerarGraph(fluxo[i].dialog_node, fluxo[i].title,fluxo[i].condition,)
             total = total + gerarGraph(fluxo[i].dialog_node, fluxo[i].title, fluxo[i].conditions, 
                 `A${Math.trunc(Math.random()*100)}`, fluxo[i+1].dialog_node, fluxo[i+1].title )
             } else {
